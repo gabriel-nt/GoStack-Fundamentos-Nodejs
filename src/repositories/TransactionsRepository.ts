c7942defb5352070bee487ed255a04ef436a3261
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2020-08-25 21:53:28 -0300

    Challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, item) => {
+      if (item.type === 'income') {
+        return total + item.value;
+      }
+
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, item) => {
+      if (item.type === 'outcome') {
+        return total + item.value;
+      }
+
+      return total;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('Type not permited');
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new Error(
+          'Invalid create outcome transaction without a valid balance',
+        );
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
